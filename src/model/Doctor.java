f68a0b398d841fa7b5674b0be3a17b3a2a93f185
A$AP_Pretty <hdvillatoro@scl.edu.gt>
2024-09-17 17:38:17 -0600

    se agregaron cambios en la carpeta -se creo una nueva rama llamada backEnd -Se crea un nuevo packpage llamado backEnd -se hizo una nueva clase

---
+++ src/model/Doctor.java
@@ -0,0 +1,71 @@
+package src.model;
+
+public class Doctor {
+
+    private String nombre;
+    private String usuario;
+    private String correo;
+    private String contraseña;
+    private String especialidad;
+
+    // Constructor
+    public Doctor(String nombre, String correo, String especialidad, String telefono, String usuario, String contraseña) {
+        this.nombre = nombre;
+        this.correo = correo;
+        this.especialidad = especialidad;
+        this.usuario = usuario;
+        this.contraseña = contraseña;
+    }
+
+    // Get y Set
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    public String getEspecialidad() {
+        return especialidad;
+    }
+
+    public void setEspecialidad(String especialidad) {
+        this.especialidad = especialidad;
+    }
+
+    public String getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(String usuario) {
+        this.usuario = usuario;
+    }
+
+    public String getContraseña() {
+        return contraseña;
+    }
+
+    public void setContraseña(String contraseña) {
+        this.contraseña = contraseña;
+    }
+
+    @Override
+    public String toString() {
+        return "Doctor{" +
+                "nombre='" + nombre + '\'' +
+                ", correo='" + correo + '\'' +
+                ", especialidad='" + especialidad + '\'' +
+                ", usuario='" + usuario + '\'' +
+                ", contraseña='" + contraseña + '\'' +
+                '}';
+    }
+}
