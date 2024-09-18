e69550440fb77cdc7720bf3c7163379ea3e523a9
A$AP_Pretty <hdvillatoro@scl.edu.gt>
2024-09-18 08:06:20 -0600

    mis cambios

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 package src;
 
 import src.controller.LoginController;
+import src.view.DoctorView;
 
 import javax.swing.*;
 import java.awt.*;
@@ -9,6 +10,7 @@ public class Main extends JFrame {
     public static void main(String[] args) {
         Login login = new Login();
         LoginController controlLogin = new LoginController(login);
+        DoctorView doctorView = new DoctorView();
 
 
 
